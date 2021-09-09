212afb478c87ea45a736dfab04560f8f81459b5d
Apoorv Panse <apoorv135@gmail.com>
2021-09-09 07:10:46 +0530

    commit

--- Apoorv/HandsOn1/05.sh
+++ Apoorv/HandsOn1/05.sh
@@ -2,12 +2,13 @@
 #MT2020013 Apoorv Panse
 #Shell Script to execute and check file descriptor status for 05.c
 
-gcc 5.c
+gcc 05.c
 ./a.out &
 PID=$!
 echo "PID of the process : $PID"
-ls -l /proc/$PID/fd/
+echo "List of FDs listed in /proc/PID/fd/"
+ls -l /proc/$PID/fd/ | grep test | awk '{print $9 $10 $11}'
 echo "Killing process : $PID"
 kill -9 $PID
-rm file* a.out
-echo "Fin."
+rm test* a.out
+echo "Done"
