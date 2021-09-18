fe06e745a83d3fe9aad21194e2512f512282209a
Apoorv Panse <apoorv135@gmail.com>
2021-09-18 17:08:30 +0530

    commit

---
+++ Apoorv/HandsOn1/26.c
@@ -0,0 +1,29 @@
+/*
+MT2020013 Apoorv Panse
+Write a program to execute an executable program.
+a. use some executable program
+b. pass some input to an executable program. (for example execute an executable of $./a.out name)
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+#include <errno.h>
+
+int main()
+{   //test file execution should print arguments 'Hello' and 'World' that is passed...
+    int x = execl("./test26.out", "Hello","World", (char *) NULL);
+    if ( x < 0 ){
+        printf("%s\n",strerror(errno));
+    }
+    
+	return 0;
+}
+
+/*
+Printing the arguments passed : 
+Hello
+World
+*/
